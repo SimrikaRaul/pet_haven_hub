fa89220ced90a7b1e3d499fcba5409c5f2588d9b
raulsimrika <raulsimrika@gmail.com>
2025-12-09 20:23:58 +0545

    feat: Add name field to user registration form and permit name parameter in registrations controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,3 +1,16 @@
 class Users::RegistrationsController < Devise::RegistrationsController
- 
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
+
+  protected
+
+  # Permit name parameter for sign up
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  # Permit name parameter for account update
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 end
