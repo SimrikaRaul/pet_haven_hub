5c6b7670e4138eada1e47d4bc711038ee3a6b63c
raulsimrika <raulsimrika@gmail.com>
2025-12-13 20:18:47 +0545

    feat: Update home controller logic, enhance navbar visibility for requests, and improve home page layout with new styles

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
-   #@featured_pets = Pet.featured
+    @featured_pets = Pet.where(available: true).recent.limit(8)
+    @total_pets = Pet.where(available: true).count
+    @total_adopted = Pet.where(available: false).count
   end
 end
--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,5 +1,5 @@
 class RequestsController < ApplicationController
-  before_action :authenticate_user!,only: [:create]
+  before_action :authenticate_user!,only: [:index, :create]
   before_action :set_pet, only: [:create]
   before_action :set_request, only: [:show, :update]
 
